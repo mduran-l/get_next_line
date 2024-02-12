eba6e6837a957be284b88f3623ff194b867090e3
Gustavo Duran <mduran-l@student.42malaga.com>
2024-02-12 14:52:54 +0100

    save

--- get_next_line.c
+++ get_next_line.c
@@ -6,62 +6,75 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 11:27:07 by mduran-l          #+#    #+#             */
-/*   Updated: 2024/02/07 14:41:18 by mduran-l         ###   ########.fr       */
+/*   Updated: 2024/02/12 14:50:50 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
-void	ft_bzero(void *s, size_t n)
+static char	*extract_line(char *buff)
 {
-	size_t	i;
+	char	*line;
+	char	l;
 
-	i = 0;
-	while (i < n)
-		((char *)s)[i ++] = 0;
+	if (!buff)
+		return (NULL);
+	l = ft_linelen(buff) + 1;
+	line = ft_substr(buff, 0, l);
+	if (!line)
+	{
+		free(buff);
+		return (NULL);
+	}
+	return (line);
 }
 
-char	*read_file(int fd, char *buff)
+static char	*clear_buffer(char *buff)
 {
-	int		rfd;
-	char	*temp;
+	size_t	l;
+	size_t	s;
+	char	*cleared;
 
-	rfd = 1;
-	temp = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	if (!temp)
-		return (free(buff), NULL);
-	while (rfd && !ft_linelen(buff))
+	if (!buff)
+		return (NULL);
+	l = ft_linelen(buff) + 1;
+	s = ft_strlen(buff);
+	if (!s)
 	{
-		ft_bzero(temp, BUFFER_SIZE + 1);
-		rfd = read(fd, temp, BUFFER_SIZE);
-		if (rfd < 0)
-			return (free(temp), NULL);
-		buff = ft_strjoin(buff, temp);
-		if (!buff)
-			return (free(temp), NULL);
+		free(buff);
+		return (NULL);
 	}
-	return (free(temp), buff);
+	cleared = ft_substr(buff, l, s);
+	if (!cleared)
+		return (NULL);
+	free(buff);
+	return (cleared);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*buff = {0};
 	char		*line;
-	size_t		len;
+	int			rfd;
 
-	if (fd < 0 || !BUFFER_SIZE)
+	rfd = 1;
+	if (!fd || !BUFFER_SIZE)
 		return (NULL);
-	if (!buff || (buff && !ft_linelen(buff)))
-	{
-		buff = read_file(fd, buff);
-		if (!buff)
-			return (NULL);
-	}
-	len = ft_linelen(buff) + 1;
-	line = ft_substr(buff, 0, len);
+	line = (char *)ft_calloc(1 + BUFFER_SIZE, sizeof(char));
 	if (!line)
-		return (free(buff), NULL);
-	buff = ft_substr(buff, len, ft_strlen(buff) - len);
-	if (!buff)
-		return (free(line), NULL);
+		return (NULL);
+	while (rfd && !ft_linelen(line))
+	{
+		rfd = read(fd, line, BUFFER_SIZE);
+		if (rfd < 0)
+		{
+			free(line);
+			return (NULL);
+		}
+		if (rfd)
+			buff = ft_strjoin(buff, line);
+	}
+	free(line);
+	line = extract_line(buff);
+	buff = clear_buffer(buff);
 	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/23 09:41:56 by mduran-l          #+#    #+#             */
-/*   Updated: 2024/02/07 14:05:50 by mduran-l         ###   ########.fr       */
+/*   Created: 2024/02/12 14:25:57 by mduran-l          #+#    #+#             */
+/*   Updated: 2024/02/12 14:48:46 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -14,13 +14,14 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*out;
+	size_t	i;
 
-	if (!count || !size)
-		return (NULL);
 	out = (void *)malloc(count * size);
 	if (!out)
 		return (NULL);
-	ft_bzero(out, count * size);
+	i = 0;
+	while (i < count * size)
+		((char *)out)[i ++] = 0;
 	return (out);
 }
 
@@ -44,9 +45,9 @@ size_t	ft_strlen(char *s)
 {
 	size_t	i;
 
-	if (!s)
-		return (0);
 	i = 0;
+	if (!s)
+		return (i);
 	while (s[i])
 		i ++;
 	return (i);
@@ -58,12 +59,12 @@ char	*ft_substr(char *s, size_t start, size_t len)
 	size_t	l;
 	char	*subs;
 
-	l = ft_strlen(s);
 	if (!s)
 		return (NULL);
+	l = ft_strlen(s);
 	if (len > l - start)
 		len = l - start;
-	subs = ft_calloc(len + 1, sizeof(char));
+	subs = ft_calloc(1 + len, sizeof(char));
 	if (!subs)
 		return (NULL);
 	i = 0;
@@ -77,27 +78,29 @@ IMPORTANT!: When execution starts, s1 has no data or allocated memory
 */
 char	*ft_strjoin(char *s1, char *s2)
 {
-	size_t	l1;
-	size_t	l2;
 	int		i;
+	int		j;
 	char	*joint;
 
 	if (!s1)
 	{
-		s1 = ft_calloc(1, sizeof(char));
+		s1 = (char *)ft_calloc(1, sizeof(char));
 		if (!s1)
 			return (NULL);
 	}
-	l1 = ft_strlen(s1);
-	l2 = ft_strlen(s2);
-	joint = ft_calloc(l1 + l2 + 1, sizeof(char));
+	joint = (char *)ft_calloc(1 + ft_strlen(s1) + ft_strlen(s2),
+			sizeof(char));
 	if (!joint)
-		return (free(s1), NULL);
+	{
+		free(s1);
+		return (NULL);
+	}
 	i = -1;
 	while (s1[++ i])
 		joint[i] = s1[i];
-	i = -1;
-	while (s2[++ i])
-		joint[l1 + i] = s2[i];
-	return (free(s1), joint);
+	j = -1;
+	while (s2[++ j])
+		joint[i + j] = s2[j];
+	free(s1);
+	return (joint);
 }
